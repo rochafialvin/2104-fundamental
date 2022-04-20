cf104d4999afe0ac1b35b4875463446c8a6b15e3
rochafialvin <rochafi.dev@gmail.com>
2022-04-20 11:58:12 +0700

    If statement

---
+++ Day2/index.js
@@ -0,0 +1,41 @@
+// IF Statement
+
+// Jika condition bernilai true, maka task akan dikerjakan
+// if(condition){
+//    task
+// }
+
+// var rain = true;
+// // Jika rain bernilai true, tampilkan "Sedang hujan, pakailah payung"
+// if (rain) {
+//   console.log("Sedang hujan, pakailah payung");
+// }
+
+// 0.0000000000000 - 0.999999999999
+// var random = Math.random();
+// // 00.000000000000 - 09.99999999999
+// random = random * 10;
+// // 0.000000000000 - 9.99999999999
+// random = Math.floor(random);
+// // 0 - 9
+// // 1 - 10
+// console.log({ random });
+
+// var guilty = false;
+// if (!guilty) {
+//   console.log("Guilty ", guilty);
+//   console.log("Anda dibebaskan");
+// }
+
+// var age = 17;
+// if (age >= 18) {
+//   console.log("Permohonan diterima, silahkan lanjut ke proses berikutnya");
+// }
+
+// var username = "rochafi"; // true
+// var email = "rochafi@gmail.com"; // true
+
+// if (username && email) {
+//   // true
+//   console.log("Register berhasil");
+// }
