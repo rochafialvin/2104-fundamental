b345cb72c4b5d597441f9b7df35fd13e04b84a60
rochafialvin <rochafi.dev@gmail.com>
2022-04-20 14:34:20 +0700

    Exercise Payment

---
+++ Day2/Exercise/payment.js
@@ -0,0 +1,10 @@
+var total = 80000;
+
+// Jika totalnya lebih dari 100rb, discount 15%
+// Jika totalnya lebih dari 70rb, discount 10%
+// Dibawah 70rb discount 5%
+
+// result
+// Total belanja : 80000
+// Total discount : 8000
+// Total yg dibayar : 72000
