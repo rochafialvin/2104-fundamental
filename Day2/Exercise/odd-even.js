cdb510b1b80293c1d9bdedb4f280ea060c0b09e8
rochafialvin <rochafi.dev@gmail.com>
2022-04-20 13:41:39 +0700

    odd even exercise

---
+++ Day2/Exercise/odd-even.js
@@ -0,0 +1,13 @@
+// Ganjil & Genap
+
+var number = 9;
+// Write you code here
+
+// Angka 9 adalah ganjil
+// Angka 10 adalah genap
+
+var day = "Sunday";
+
+console.log("Today is " + day);
+
+console.log(`Today is ${day}`);
