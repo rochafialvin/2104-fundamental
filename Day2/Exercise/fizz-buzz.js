d90c12324f688a08a57cf894124411f52cfa7369
rochafialvin <rochafi.dev@gmail.com>
2022-04-21 15:09:44 +0700

    Exercise Fizz Buzz

---
+++ Day2/Exercise/fizz-buzz.js
@@ -0,0 +1,21 @@
+//////////////
+// FIZZ BUZZ
+/////////////
+
+/*
+    Jika suatu angka dapat dibagi 3. Gantikan dengan kata Fizz
+
+    Jika suatu angka dapat dibagi 5. Gantikan dengan kata Buzz
+
+    Jika suatu angka dapat dibagi 3 dan 5. Gantikan dengan kata FizzBuzz
+
+    Jika kita ingin mencari nilai yang dapat dibagi oleh dua angka tertentu, maka bagilah dengan angka hasil perkalian dari dua nilai tersebut.
+
+    FizzBuzz akan muncul saat suatu angka dapat dibagi 15 .
+*/
+
+var number = 15;
+
+// you code start here
+
+// show : 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz
