297a6087f132c5e3f55d1953757b98f1d2d9a4fb
rochafialvin <rochafi.dev@gmail.com>
2022-04-22 10:28:54 +0700

    Nested For loop

--- Day3/2.for-loop.js
+++ Day3/2.for-loop.js
@@ -25,13 +25,37 @@
 //   console.log("Angka", number);
 // }
 
-for (var i = 1; i < 6; i++) {
-  if (i % 2) {
-    console.log(`Angka ${i} adalah ganjil`);
+// for (var i = 1; i < 6; i++) {
+//   if (i % 2) {
+//     console.log(`Angka ${i} adalah ganjil`);
+//   }
+// }
+
+// Penjumlahan
+// angka + angka = angka
+// string + string = string
+// angka + string = string
+
+// number = 3
+for (var number = 1; number < 3; number++) {
+  var word = ""; // "1"
+  // i = 2
+  for (var i = 1; i < number; i++) {
+    // "" + 1 = "1"
+    word += i;
   }
+  console.log(word);
 }
 
-// Angka 1
-// Angka 2
-// Angka 3
-// Angka 4
+// var word = "Javascript";
+// var words = word.split("");
+// var wordsReversed = [];
+// for (var i = word.length - 1; i >= 0; i--) {
+//   wordsReversed.push(words[i]);
+// }
+
+// console.log(word);
+// console.log(wordsReversed.join(""));
+
+// var word2 = "Javascript";
+// console.log(word2.split("").reverse().join(""));
