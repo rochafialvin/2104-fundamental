2235384c8d8fd36763076b6437b3efa51540bfad
rochafialvin <rochafi.dev@gmail.com>
2022-05-09 16:41:03 +0700

    Leh oleh highest and lowest

---
+++ Day4/Exercise/min-max.js
@@ -0,0 +1,10 @@
+var nums = [2, 5, 3];
+var min, max;
+
+nums.forEach((val, index) => {
+  // Lanjutkan bang
+});
+
+console.log(`Min : ${min}\nMax : ${max}`);
+// [2, 5, 3]; min : 2 , max : 5
+// [5]; min : 5, max : 5
