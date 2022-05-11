4fe61e4a49612e3f688fb5d4fb88c09687d80a57
rochafialvin <rochafi.dev@gmail.com>
2022-05-11 14:12:12 +0700

    Exercise reverse word

---
+++ Day6/Exercise/1 reverse-word.js
@@ -0,0 +1,12 @@
+var text = "My name is Alex";
+
+var reverseWord = (word) => {
+  // kerjakan disini
+};
+
+var result = reverseWord(text);
+console.log(result); // "Alex is name My"
+
+var tulisan = "Purwadhika";
+var splittedTulisan = tulisan.split(""); // [ 'P', 'u', 'r', 'w', 'a', 'd', 'h', 'i','k', 'a']
+splittedTulisan.join(""); //"Purwadhika"
