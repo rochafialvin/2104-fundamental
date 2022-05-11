d123cc446fbec62c722541ab2798542e0e827847
rochafialvin <rochafi.dev@gmail.com>
2022-05-11 10:20:55 +0700

    Add more errors on note

---
+++ Day6/2 string-props-methds.js
@@ -0,0 +1,31 @@
+// STRING PROPERTIES AND METHODS //
+
+// var x = "Hello World!";
+
+// Banyak karakter pada strings
+// console.log(x.length); // 12
+
+// Mencari index dari string World di dalam string X
+// console.log(x.indexOf("World!")); // 6
+
+// Mencopy bagian tertentu dari strings
+// index awal, index akhir (tidak termasuk)
+// console.log(x.slice(5, 8)); // 5 - 7
+
+// Memecah string menjadi beberapa bagian berpatokan dengan karakter tertentu
+// console.log(x.split(" ")); // ["Hello", "World"]
+// console.log(x.split("o")); // ["Hell", " W", "rld"]
+
+// var splittedWord = x.split(" ");
+// console.log(splittedWord.length); // 2
+
+var x = "Hello World!";
+// Mengubah string menjadi huruf kapital
+var uppercaseX = x.toUpperCase();
+console.log(x); // "Hello World!"
+console.log(uppercaseX); // "HElLO WORLD!"
+
+// Mengubah string menjadi huruf kecil
+var lowercaseX = x.toLowerCase();
+console.log(x); // "Hello World!"
+console.log(lowercaseX); // "hello world!"
