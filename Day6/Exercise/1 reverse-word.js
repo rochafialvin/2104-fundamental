9ccbe601e7bbcd0434aff15b26a66182561ae02d
rochafialvin <rochafi.dev@gmail.com>
2022-05-11 15:58:21 +0700

    Solved reverse word

--- Day6/Exercise/1 reverse-word.js
+++ Day6/Exercise/1 reverse-word.js
@@ -1,12 +1,22 @@
 var text = "My name is Alex";
 
 var reverseWord = (word) => {
-  // kerjakan disini
+  // word : "My name is Alex"
+  // Merubah word menjadi array : ["My", "name", "is", "Alex"]
+  var splittedWord = word.split(" ");
+  // Proses data pada array satu per satu, setiap data akan di reverse
+  // ["yM", "eman", "si", "xelA"]
+  var reversedSplittedWord = splittedWord.map((letter) => {
+    // letter : "My" --> ["M", "y"] --> ["y", "M"] --> "yM"
+    var splittedLetter = letter.split(""); // ["M", "y"]
+    splittedLetter.reverse(); // ["y", "M"]
+    var joinedLetter = splittedLetter.join(""); // "yM"
+    return joinedLetter; // "yM"
+  });
+  // Array yang sudah di reverse akan dijadikan string kembali
+  // Keluarkan string dari function ini
+  return reversedSplittedWord.join(" ");
 };
 
 var result = reverseWord(text);
-console.log(result); // "Alex is name My"
-
-var tulisan = "Purwadhika";
-var splittedTulisan = tulisan.split(""); // [ 'P', 'u', 'r', 'w', 'a', 'd', 'h', 'i','k', 'a']
-splittedTulisan.join(""); //"Purwadhika"
+console.log(result); // "yM eman si xelA"
