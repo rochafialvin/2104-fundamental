e55b0f187971d1c08805ba63ccd1516abb921572
rochafialvin <rochafi.dev@gmail.com>
2022-05-17 14:02:14 +0700

    InnerHTML

--- Day9/form/form.js
+++ Day9/form/form.js
@@ -1,4 +1,5 @@
 const createInfo = () => {
+  // type = text
   // akses element yang memiliki id = "name"
   const input = document.getElementById("name");
   // yang diketik di dalam textbox akan tersimpan di property value
@@ -6,7 +7,7 @@ const createInfo = () => {
 
   const price = document.getElementById("price").value;
 
-  // Radio button
+  // type = radio
   // Ambil satu Tag input yang memiliki name condition dan di ceklis
   // querySelector return 1 element object
   const condition = document.querySelector(
@@ -19,6 +20,11 @@ const createInfo = () => {
   // querySelectorAll return 1 array berisi element object
   // All --> array
   const couriers = document.querySelectorAll(`input[name=courier]:checked`);
+  // couriers = [<input>, <input>]
+  // couriers[0] = <input>.value --> couriers[0].value
+  // couriers[1] = <input>.value --> couriers[1].value
 
-  console.log({ name, price, condition, warehouse, couriers });
+  const p = document.getElementById("result");
+  // innerText tidak mengenal tag HTML. innerHTML kenal.
+  p.innerHTML = "Berubah !1!1";
 };
