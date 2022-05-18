0b53e4d5930ba6bf949f33b408155b2342c6eb41
rochafialvin <rochafi.dev@gmail.com>
2022-05-18 09:46:09 +0700

    Input data

--- Day9/market/market.js
+++ Day9/market/market.js
@@ -1,4 +1,12 @@
-// const time = new Date();
+class Product {
+  constructor(id, name, price, stock) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.stock = stock;
+  }
+}
+
 const products = [
   { id: 1652777173833, name: "Celana Jin", price: 120000, stock: 30 },
   { id: 1652777222715, name: "Hoodie Jin", price: 150000, stock: 20 },
@@ -18,6 +26,25 @@ const fnRenderList = () => {
   });
 
   document.getElementById("tableBody").innerHTML = listProduct.join("");
+};
+
+// Hanya akan dipanggil jika di tekan tombol input
+const fnInputData = () => {
+  // Ambil informasi mengenai product yang ingin ditambahkan
+  const name = document.getElementById("name").value;
+  const price = document.getElementById("price").value;
+  const stock = document.getElementById("stock").value;
+  // Buat object dengan informasi yang di dapat
+  const time = new Date();
+  const id = time.getTime();
+  // const product = new Product(id, name, price, stock);
+  const product = { id, name, price, stock };
+  // Masukkan object ke array products
+  products.push(product);
+  // reset form --> mengosongkan semua textbox
+  document.getElementById("form").reset();
+  // Mengupdate list data di table data
+  fnRenderList();
 };
 
 fnRenderList();
